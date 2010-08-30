d906dbd649ecd6606551ffbd9050245b5dfe4e71
xavier <xmuselet@brandonlinecommerce.com>
2010-08-30 09:27:55 +0200

    Début de design HP : Création d'un design HR Création d'un skin HR Création d'un module pour le menu dans le footer

---
+++ app/code/local/Hr/Page/Block/Html/FooterMenu.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Html page block
+ *
+ * @category   Hr
+ * @package    Hr_Page
+ * @author     Xavier <xmuselet@brandonlinecommerce.com>
+ */
+class Hr_Page_Block_Html_FooterMenu extends Mage_Core_Block_Template
+{
+}
