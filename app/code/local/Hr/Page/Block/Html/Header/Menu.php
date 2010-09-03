21db4c56c0ee26cf83962c8c96ed7186b0a9b1b4
Xavier Muselet <xmuselet@brandonlinecommerce.com>
2010-09-03 16:26:57 +0200

    Design de la barre de menu dans le header Ajout du logo HR Ajout de l'image du panier

--- app/code/local/Hr/Page/Block/Html/Header/Menu.php
+++ app/code/local/Hr/Page/Block/Html/Header/Menu.php
@@ -6,6 +6,6 @@
  * @package    Hr_Page
  * @author     Xavier <xmuselet@brandonlinecommerce.com>
  */
-class Hr_Page_Block_Html_FooterMenu extends Mage_Core_Block_Template
+class Hr_Page_Block_Html_Header_Menu extends Mage_Core_Block_Template
 {
 }
