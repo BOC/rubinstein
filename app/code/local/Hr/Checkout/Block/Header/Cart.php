c3d4daa9d94fc58a0b6039cb9dc0c2afef49f63d
Xavier Muselet <xmuselet@brandonlinecommerce.com>
2010-09-06 18:20:08 +0200

    insertion du block panier dans le header avec le module associé

---
+++ app/code/local/Hr/Checkout/Block/Header/Cart.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Shopping cart block
+ *
+ * @category    Hr
+ * @package     Hr_Checkout
+ * @author      Xavier Muselet <xmuselet@brandonlinecommerce.com>
+ */
+class Hr_Checkout_Block_Header_Cart extends Mage_Checkout_Block_Cart
+{
+	
+}
