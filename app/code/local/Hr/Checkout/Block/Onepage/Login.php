3a9708fac6e72469552ffdc7bf3dff20f883ffdf
Xavier Muselet <xmuselet@brandonlinecommerce.com>
2010-10-04 18:22:14 +0200

    Design et navigation dans le checkout

---
+++ app/code/local/Hr/Checkout/Block/Onepage/Login.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * One page checkout status
+ *
+ * @category   Hr
+ * @package    Hr_Checkout
+ * @author     Xavier Muselet <xmuselet@brandonlinecommerce.com>
+ */
+class Hr_Checkout_Block_Onepage_Login extends Mage_Checkout_Block_Onepage_Login
+{
+    protected function _construct()
+    {
+    	parent::_construct();
+        if (!$this->isCustomerLoggedIn()) {
+            $this->getCheckout()->setStepData('login', array('label'=>Mage::helper('checkout')->__('Connection/registration'), 'allow'=>true));
+        }
+    }
+}
